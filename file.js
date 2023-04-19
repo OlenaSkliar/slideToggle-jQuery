15ef59e5fda63db05d9e9c19fded28a6ac4fb5a6
OlenaSkliar <99468750+OlenaSkliar@users.noreply.github.com>
2023-04-20 00:52:54 +0300

    delete close script

--- file.js
+++ file.js
@@ -3,7 +3,3 @@ $(".slideToggle").click(function (e) {
     $(".slideBox").slideToggle();
     $(".slideToggle").toggleClass('active')
 });
-$(".slideBox .close").click(function (e) {
-    $(".slideBox").slideToggle();
-    $(".slideToggle").toggleClass('active')
-});
